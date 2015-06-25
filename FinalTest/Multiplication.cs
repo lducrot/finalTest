2de0b40dd7a399200fc2d6d48ad0c635d458206f
unknown <p1413925@IUTDOUAB2PC16.univ-lyon1.fr>
2015-06-25 09:14:18 +0200

    Test DéfinirUneClasseRealisantUneMultiplication passé

---
+++ FinalTest/Multiplication.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FinalTest.Tests
+{
+    public class Multiplication
+    {
+        public Multiplication()
+        {
+        }
+
+        public Boolean PeutCalculer(string chaine)
+        {
+            Boolean ok = false;
+            if (chaine.Substring(1, 1) == "*")
+            {
+                ok = true;
+            }
+            else ok = false;
+            return ok;
+        }
+
+        public Int32 Calculer(String chaine)
+        {
+            int resultat = 0;
+            int a = 0;
+            int b = 0;
+            Int32.TryParse(chaine.Substring(0,1), out a);
+            Int32.TryParse(chaine.Substring(2, 1), out b);
+
+            resultat = a * b;
+            return resultat;
+        }
+
+    }
+}
---
+++ FinalTest/TypeReference.cs
@@ -0,0 +1,11 @@
+﻿namespace FinalTest.Tests
+{
+    public class TypeReference
+    {
+        private int i;
+        public TypeReference(int i)
+        {
+            this.i = i;
+        }
+    }
+}
