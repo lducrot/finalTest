1ef9798dba0dbc6ed903514f2a15110b31da7c3c
unknown <p1413925@IUTDOUAB2PC16.univ-lyon1.fr>
2015-06-25 10:33:48 +0200

    test RécupérerLesNombresPairs passé

---
+++ FinalTest/Nombres.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace FinalTest.Tests
+{
+    public class Nombres
+    {
+        private readonly IEnumerable<KeyValuePair<string, int>> _keyValuePairs;
+
+        public IEnumerable<int> NombresPairs {
+            get { return _keyValuePairs.Where(pair => pair.Value%2==0).Select(pair => pair.Value); } 
+        }
+
+        public Nombres(IEnumerable<KeyValuePair<string, int>> keyValuePairs)
+        {
+            _keyValuePairs = keyValuePairs;
+        }
+    }
+}
